c11aee1937c1661670c5adcba4e35b42a9db2fbd
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-03-30 16:17:28 +0200

    5 ft

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    new_mat = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        new_mat.append(row)
+    return new_mat
