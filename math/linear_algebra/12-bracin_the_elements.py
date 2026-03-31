ab6b0a013a8d500a2c3b1e86d66511254b008865
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-03-31 11:47:09 +0200

    12 ft

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""performs basic operations"""
+
+
+def np_elementwise(mat1, mat2):
+    """return elementary operations on 2 matrices"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
