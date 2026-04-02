2a294efda54ed9310099ddfea377aba4da98d48f
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-04-02 12:54:57 +0200

    4 ft

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""History chart"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """History chart of students with grades"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.xlabel('Grades')
+    plt.ylabel('Number of students')
+    plt.title('Project A')
+    plt.show()
