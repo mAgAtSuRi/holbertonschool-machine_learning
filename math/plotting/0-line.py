2b0a85add12e0c779bd1fddb7b641a266df89eac
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-04-02 10:24:44 +0200

    doc

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""Draw a linear graph in red"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    """Draw a red line"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
