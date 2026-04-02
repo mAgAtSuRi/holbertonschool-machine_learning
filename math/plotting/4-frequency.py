e42799539d9d6ef2a004994a8b06bb5649ea1585
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-04-02 23:02:29 +0200

    test xticks

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,7 +13,10 @@ def frequency():
     plt.hist(student_grades, bins=range(0, 110, 10), edgecolor='black')
     plt.xlim(0, 100)
     plt.ylim(0, 30)
+    plt.xticks(range(0, 110, 10))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
     plt.show()
+
+frequency()
