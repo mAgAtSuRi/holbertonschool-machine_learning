1719a964cffb0372a1fae8d54bc7dd6c713f4fac
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-04-02 12:44:49 +0200

    3 ft

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -15,8 +15,9 @@ def two():
     plt.figure(figsize=(6.4, 4.8))
 
     plt.plot(x, y1, "r--", label="C-14")
-    plt.plot(x, y2, "g", label="Ra-226")
+    plt.plot(x, y2, "g-", label="Ra-226")
     plt.xlim(0, 20000)
+    plt.ylim(0, 1)
     plt.xlabel("Time (years)")
     plt.ylabel("Fraction Remaining")
     plt.title("Exponential Decay of Radioactive Elements")
