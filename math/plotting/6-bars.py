4fa984534b769f7414ef80fa465bc14c3d58d981
Tristan <tristan.latuilliere@gmail.com>
2026-04-03 10:50:18 +0200

    colors added

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -12,11 +12,15 @@ def bars():
     bottom = np.zeros(3)
     names = ["Farrah", "Fred", "Felicia"]
     labels = ["apples", "bananas", "oranges", "peaches"]
-    colors = ["red", "yellow", "orange", "peachpuff"]
+    colors = ["red", "yellow", "#ff8000", "#ffe5b4"]
     for i, row in enumerate(fruit):
-        plt.bar(names, row, bottom=bottom, color=colors[i], label=labels[i])
+        plt.bar(names, row, bottom=bottom, color=colors[i], label=labels[i], width=0.5)
         bottom += row
     plt.ylabel("Quantity of Fruit")
     plt.ylim(0, 80)
+    plt.yticks(range(0, 81, 10))
     plt.legend()
+    plt.title('Number of Fruit per Person')
     plt.show()
+
+bars()
