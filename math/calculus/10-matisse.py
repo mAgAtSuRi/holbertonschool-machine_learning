922d7373296c282a8e587de58d2a3655c4b4a130
Tristan <tristan.latuilliere@gmail.com>
2026-04-10 10:22:07 +0200

    list

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,5 +6,5 @@ def poly_derivative(poly):
     if len(poly) == 0 or not isinstance(poly, list):
         return None
     if len(poly) == 1:
-        return 0
+        return [0]
     return [i * poly[i] for i in range(1, len(poly))]
