6790d21bb7130d5bafbfbfe467e2f59149d3ab35
Tristan <tristan.latuilliere@gmail.com>
2026-04-10 10:05:20 +0200

    test

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """calculate the sum of i ** 2"""
-    if type(n) is not int:
+    if not isinstance(n, int) or n < 1:
         return None
-    return (n * (n + 1) * (2 * n + 1)) // 6
+    return n * (n + 1) * (2 * n + 1) // 6
